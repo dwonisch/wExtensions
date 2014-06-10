abe537ffece9d94a54f610d9b88cc662359e58c4
Daniel Wonisch <daniel.wonisch@gmail.com>
2014-06-10 21:42:57 +0200

    Raising event extensions
    Should provide methods to synchronize events when used within multiple
    threads.

---
+++ wExtensions.UnitTests/EventTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace wExtensions.UnitTests {
+
+    [TestFixture]
+    public class EventTests {
+        private event EventHandler TestEvent;
+        private event EventHandler<EventArgs> GenericTestEvent;
+
+        [Test]
+        public void EventIsRaised() {
+            int wasCalled = 0;
+
+            TestEvent += (sender, args) => wasCalled++;
+
+            TestEvent.Raise(null, null);
+
+            Assert.That(wasCalled, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void GenericEventIsRaised() {
+            int wasCalled = 0;
+
+            GenericTestEvent += (sender, args) => wasCalled++;
+
+            GenericTestEvent.Raise(null, null);
+
+            Assert.That(wasCalled, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void RaiseEventWithEmptyArgs() {
+            EventArgs eventArgs = null;
+
+            TestEvent += (sender, args) => eventArgs = args;
+
+            TestEvent.Raise(this);
+
+            Assert.That(eventArgs, Is.EqualTo(EventArgs.Empty));
+        }
+    }
+}
---
+++ wExtensions/EventExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace System {
+	public static class EventExtensions {
+	    public static void Raise(this EventHandler @event, object sender) {
+	        Raise(@event, sender, EventArgs.Empty);
+	    }
+
+	    public static void Raise(this EventHandler @event, object sender, EventArgs args) {
+	        if (@event != null)
+	            @event(sender, args);
+	    }
+
+        public static void Raise<T>(this EventHandler<T> @event, object sender, T args) where T : EventArgs {
+            if (@event != null)
+                @event(sender, args);
+        }
+    }
+}
